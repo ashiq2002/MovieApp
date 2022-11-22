6f45d00324c0bd7dc98d116eb478c5cd1bc3ca6f
ashiq2002 <mdashiqhossain6@gmail.com>
2022-11-22 12:58:34 +0600

    E-Student

--- EStudent2/app/src/main/java/com/setbitzero/e_student/adapters/FirebaseAdapter.java
+++ EStudent2/app/src/main/java/com/setbitzero/e_student/adapters/FirebaseAdapter.java
@@ -2,13 +2,13 @@ package com.setbitzero.e_student.adapters;
 
 
 import android.content.Context;
-import android.content.DialogInterface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -23,6 +23,8 @@ import com.orhanobut.dialogplus.DialogPlus;
 import com.setbitzero.e_student.R;
 import com.setbitzero.e_student.models.StudentModel;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class FirebaseAdapter extends FirebaseRecyclerAdapter<StudentModel, FirebaseAdapter.ViewHolder> {
@@ -43,7 +45,7 @@ public class FirebaseAdapter extends FirebaseRecyclerAdapter<StudentModel, Fireb
         //holder.binding.deleteBtn.setOnClickListener(view-> Toast.makeText(view.getContext(), "Delete "+position, Toast.LENGTH_LONG).show());
         //holder.binding.editBtn.setOnClickListener(view-> Toast.makeText(view.getContext(), "Edit "+position, Toast.LENGTH_LONG).show());
 
-        holder.editBtn.setOnClickListener(v-> updateDialog(model));
+        holder.editBtn.setOnClickListener(v-> updateDialog(model, position));
         holder.deleteBtn.setOnClickListener(v-> delete(position));
     }
 
@@ -80,39 +82,66 @@ public class FirebaseAdapter extends FirebaseRecyclerAdapter<StudentModel, Fireb
 
     }
 
-    private void updateDialog(StudentModel model){
-        EditText name, age, department, semester;
-        Button submitBtn;
+    private void updateDialog(StudentModel model, int position){
+        TextInputEditText name, age, department, semester;
+        Button updateBtn;
         DialogPlus dialogPlus = DialogPlus.newDialog(context)
                 .setContentHolder(new com.orhanobut.dialogplus.ViewHolder(R.layout.update_layout))
                 .setExpanded(true, 800)
                 .create();
 
-//        View view = dialogPlus.getHeaderView();
-//        name = view.findViewById(R.id.uName);
-//        age = view.findViewById(R.id.uAge);
-//        department = view.findViewById(R.id.uDepartment);
-//        semester = view.findViewById(R.id.uSemester);
-//
-//        name.setText(model.getName());
-//        age.setText(model.getAge());
-//        department.setText(model.getDepartment());
-//        semester.setText(model.getSemester());
+        View view = dialogPlus.getHolderView();
+        name = view.findViewById(R.id.uName);
+        age = view.findViewById(R.id.uAge);
+        department = view.findViewById(R.id.uDepartment);
+        semester = view.findViewById(R.id.uSemester);
+        updateBtn = view.findViewById(R.id.updateButton);
+
+        name.setText(model.getName());
+        age.setText(model.getAge());
+        department.setText(model.getDepartment());
+        semester.setText(model.getSemester());
 
         dialogPlus.show();
+        updateBtn.setOnClickListener(v->update(dialogPlus, new TextInputEditText[]{name, age, department, semester}, position));
+
+    }
+
+    private void update(DialogPlus dialogPlus, TextInputEditText[] textInputEditTexts, int position) {
+        Map<String, Object> map = new HashMap<>();
+
+        if(!Objects.requireNonNull(textInputEditTexts[0].getText()).toString().trim().isEmpty()
+                && !Objects.requireNonNull(textInputEditTexts[1].getText()).toString().trim().isEmpty()
+                && !Objects.requireNonNull(textInputEditTexts[2].getText()).toString().trim().isEmpty()
+                && !Objects.requireNonNull(textInputEditTexts[3].getText()).toString().trim().isEmpty()
+        ){
+            map.put("name", textInputEditTexts[0].getText().toString());
+            map.put("age", textInputEditTexts[1].getText().toString());
+            map.put("department", textInputEditTexts[2].getText().toString());
+            map.put("semester", textInputEditTexts[3].getText().toString());
+
+            FirebaseDatabase.getInstance().getReference().child("StudentDetails")
+                    .child(Objects.requireNonNull(getRef(position).getKey())).updateChildren(map)
+                    .addOnSuccessListener(unused -> dialogPlus.dismiss())
+
+                    .addOnFailureListener(e -> {
+                        Log.wtf("Fail", "Error : "+e);
+                        dialogPlus.dismiss();
+                    });
+
+        }else {
+            Toast.makeText(context, "All data must be entered!", Toast.LENGTH_LONG).show();
+        }
     }
 
     private void delete(int position){
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle("Delete data")
                 .setMessage("Delete?")
-                .setPositiveButton("yes", (dialog, which) -> {
-                    FirebaseDatabase.getInstance()
-                            .getReference()
-                            .child("StudentDetails")
-                            .child(Objects.requireNonNull(getRef(position).getKey())).removeValue();
-
-        })
+                .setPositiveButton("yes", (dialog, which) -> FirebaseDatabase.getInstance()
+                        .getReference()
+                        .child("StudentDetails")
+                        .child(Objects.requireNonNull(getRef(position).getKey())).removeValue())
 
                 .setNegativeButton("no", (dialog, which) -> { });
         builder.show();
